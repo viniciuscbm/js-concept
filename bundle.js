ebdf47449421a19fd368b84bde2021f3678f44dc
Vinicius Camboim <vini.camboim9@gmail.com>
2020-10-09 14:48:23 -0300

    adiciona um exemplo de default params

--- bundle.js
+++ bundle.js
@@ -2,4 +2,10 @@
 
 var name = "Vinicius";
 var age = "28";
-console.log("Meu nome é " + name + " e eu tenho " + age + " anos.");
+
+var sayMyName = function sayMyName(name) {
+  var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
+  console.log("Meu nome é " + name + " e eu tenho " + age + " anos.");
+};
+
+sayMyName(name, age);
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const name = "Vinicius"
 const age = "28"
 
-console.log("Meu nome é " + name + " e eu tenho " + age + " anos.")
+const sayMyName = function(name, age = 20){
+  console.log("Meu nome é " + name + " e eu tenho " + age + " anos.")
+}
+
+sayMyName(name, age)
