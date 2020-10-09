888b720e5d12f83420e2b83b5be8355dfe6e42f6
Vinicius Camboim <vini.camboim9@gmail.com>
2020-10-09 14:36:47 -0300

    adiciona um exemplo de const e let

--- bundle.js
+++ bundle.js
@@ -1,4 +1,29 @@
+"use strict";
+
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
 // 1 - Const
 // 2 - Let
 // 3 - Escope
-"use strict";
+var item = 1;
+item = (_readOnlyError("item"), 5);
+console.log(item); /////////
+// const item = { name: "vincius" }
+// item.name = "catha"
+// console.log(item )
+///////
+// const arr = [1, 2, 3]
+// arr.push(4)
+// console.log(arr)
+///////
+// var number = 5
+// {
+//   var number = 10
+//   console.log(number)
+// }
+// console.log(number)
+///////
+// function add(number){
+//   return number
+// }
+// console.log(add(number))
--- main.js
+++ main.js
@@ -1,3 +1,45 @@
 // 1 - Const
 // 2 - Let
 // 3 - Escope
+
+
+const item = 1
+
+item = 5
+
+console.log(item)
+
+/////////
+
+// const item = { name: "vincius" }
+
+// item.name = "catha"
+
+// console.log(item )
+
+///////
+
+// const arr = [1, 2, 3]
+
+// arr.push(4)
+
+// console.log(arr)
+
+///////
+
+// var number = 5
+
+// {
+//   var number = 10
+//   console.log(number)
+// }
+
+// console.log(number)
+
+///////
+
+// function add(number){
+//   return number
+// }
+
+// console.log(add(number))
