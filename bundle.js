6bee979b2bae1a685f26e3a3c30dd27ce83d51a1
Vinicius Camboim <vini.camboim9@gmail.com>
2020-10-09 13:52:43 -0300

    adiciona exemplo de arrow function

--- bundle.js
+++ bundle.js
@@ -1,4 +1,7 @@
-// 1 - Const
-// 2 - Let
-// 3 - Escope
 "use strict";
+
+var arr = [1, 2, 3, 5, 6, 7];
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-// 1 - Const
-// 2 - Let
-// 3 - Escope
+const arr = [1, 2, 3, 5, 6, 7]
+
+const newArr = arr.map(function(item) {
+  return item * 2
+ })
+
+ console.log (newArr)
